03babba723d61590c83b97854964f161ceb558b9
robertovargash <robertovargashdez@gmail.com>
2021-03-27 16:45:32 -0400

    cambiooo

--- resources/views/contacttypes/edit.blade.php
+++ resources/views/contacttypes/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 @section('content')
 <div class="content-wrapper">
-  <!-- Content Header (Page header) -->
+  <!-- Content Header (Page header) combiecito -->
   <div class="content-header">
     <div class="container">
       <div class="row mb-2">
